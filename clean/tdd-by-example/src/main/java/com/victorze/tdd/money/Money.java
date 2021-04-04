754c631f60567b6d7a7bdba46107527102a69fb6
Victor <victorzevallos@protonmail.com>
2021-04-04 11:59:47 -0500

    The Root of all Evil

--- clean/tdd-by-example/src/main/java/com/victorze/tdd/money/Money.java
+++ clean/tdd-by-example/src/main/java/com/victorze/tdd/money/Money.java
@@ -1,6 +1,6 @@
 package com.victorze.tdd.money;
 
-abstract public class Money {
+public class Money {
 
     protected int amount;
     protected String currency;
@@ -11,23 +11,30 @@ abstract public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public String currency() {
         return currency;
     }
 
-    abstract Money times(int multiplier);
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 
 }
--- clean/tdd-by-example/todo.md
+++ clean/tdd-by-example/todo.md
@@ -14,7 +14,7 @@
 * [x] 5 CHF * 2 = 10 CHF
 * [ ] Dollar/Franc duplication
 * [x] Common equals
-* [ ] Common times
+* [x] Common times
 * [x] Compare Francs to Dollars
 * [x] Currency?
 * [ ] Delete testFrancMultiplication?
